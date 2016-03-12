43329b2c35c1702eb60e21f403acbcaaf3f812e6
DizzyDez <desc0137@algonquinlive.com>
2016-03-12 09:05:21 -0500

    targeted each. still figuring out if statement

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var $belowHeading = $('<h2>');
+var $aboveHeading = $('<h2>');
+var $body = $('body');
+var $list = $('li');
+var $belowHeading = $('<h2>');
+var $aboveHeading = $('<h2>');
+var $belowUl = $('<ul>');
+var $aboveUl = $('<ul>');
+
+$belowHeading.html('Below ground veggies');
+
+$aboveHeading.html('Above ground veggies');
+
+$body.append($belowHeading);
+$body.append($belowUl);
+
+$body.append($aboveHeading);
+$body.append($aboveUl);
+
+$li.each(function () {
+  if ($(this).hasClass('below')) {
+    $belowUl.append($(this));
+  } else {
+    $aboveUl.append($(this));
+  }
+
+});
